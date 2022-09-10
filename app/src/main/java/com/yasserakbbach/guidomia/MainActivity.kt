e85fc0e15da086cc642fc017f21775eecf404a31
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-09-10 13:41:26 +0100

    add needed dependencies and resources

--- app/src/main/java/com/yasserakbbach/guidomia/MainActivity.kt
+++ app/src/main/java/com/yasserakbbach/guidomia/MainActivity.kt
@@ -3,13 +3,6 @@ package com.yasserakbbach.guidomia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.yasserakbbach.guidomia.ui.theme.GuidomiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GuidomiaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GuidomiaTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/yasserakbbach/guidomia/ui/theme/Dimen.kt
@@ -0,0 +1,5 @@
+package com.yasserakbbach.guidomia.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val Line = 4.dp
